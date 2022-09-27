604d915b6d3d1e1f3d33ed61c7a2879a0774ed70
vovan-zt <predator_vovan@ukr.net>
2022-09-27 22:29:02 +0300

    fix

--- index.js
+++ index.js
@@ -1,99 +1,58 @@
-// const dog = {
-//    species: "dog",
-//    name: "Rasti",
-//    gender: "male",
-//    legs: 4,
-//    hands: 0,
-//    saying: "woof-woof!",
-// };
-   
-// const cat = {
-//    species: "cat",
-//    name: "Luna",
-//    gender: "female",
-//    legs: 4,
-//    hands: 0,
-//    saying: "mur",
-// };
-   
-// const woman = {
-//    species: "human",
-//    name: "Lena",
-//    lastName: 'Jons',
-//    children: 2,
-//    gender: "female",
-//    legs: 2,
-//    hands: 2,
-//    saying: "Hello",
-// };
-   
-// const man = {
-//    species: "human",
-//    name: "Alex",
-//    gender: "male",
-//    legs: 2,
-//    hands: 2,
-//    saying: "I want to sleep",
-// };
-   
-// const catWoman = {
-//    species: "catwoman",
-//    name: "Selina",
-//    gender: "female",
-//    legs: 2,
-//    hands: 2,
-//    saying: cat.saying,
-// };
-
-// const WorldInhabitant = [dog, cat, woman, man, catWoman];
-
-// const  WorldInhabitantKeys =  ['species', 'name', 'gender', 'legs', 'hands', 'saying'];
-
-// const WorldInhabitantPrint = WorldInhabitant.map(inhabitant => WorldInhabitantKeys.map(meaning => inhabitant[meaning]));
-
-// WorldInhabitantPrint.forEach(inhabitant => {
-//    print(inhabitant.join('; '))
-// });
-
 class WorldInhabitant {
-   constructor(species, name, gender, saying) {
-      this.species = species;
-      this.name = name;
-      this.gender = gender;
-      this.saying = saying;
-      this.prop = [species, name, gender, saying];
-   }
+  constructor(species, name, gender, saying) {
+    this.species = species;
+    this.name = name;
+    this.gender = gender;
+    this.saying = saying;
+    this.prop = [species, name, gender, saying];
+  }
 
-   getMeaning() {
-      return this.prop.map(meaning => meaning);
-   }    
+  getMeaning() {
+    return this.prop.map((meaning) => meaning).join("; ");
+  }
+}
+class Human extends WorldInhabitant {
+  constructor(name, gender, saying, friends = null) {
+    super("human", name, gender, saying);
+    this.legs = 2;
+    this.hands = 2;
+    this.friends = friends;
+    this.prop = [...this.prop, this.legs, this.hands, this.friends];
+  }
+}
+class Animal extends WorldInhabitant {
+  constructor(species, name, gender, saying) {
+    super(species, name, gender, saying);
+    this.paws = 4;
+    this.prop = [...this.prop, this.paws];
+  }
+}
+class Dog extends Animal {
+  constructor(name, gender) {
+    super("dog", name, gender, "woof!");
+  }
+}
+class Cat extends Animal {
+  constructor(name, gender) {
+    super("cat", name, gender, "meow!");
+  }
+}
+class CatWoman extends Human {
+  constructor(name) {
+    super(name, 'woman', cat.saying);
+  }
 }
 
-class Human extends WorldInhabitant { 
-   constructor(species, name, gender, saying, legs = 2, hands = 2, friends = '') {
-      super(species, name, gender, saying);
-      this.legs = legs;
-      this.hands = hands;
-      this.prop = [species, name, gender, saying, legs, hands, friends];
-   }
-}
+const man = new Human("Jon", "male", "I want to sleep");
 
-class Animal extends WorldInhabitant { 
-   constructor(species, name, gender, saying, paws = 4) {
-      super(species, name, gender, saying);
-      this.paws = paws;
-      this.prop = [species, name, gender, saying, paws];
-   }
-}
+const woman = new Human("Lena", "female", "Hello", man.name);
 
- const man = new Human('human', 'Jon', 'male', 'I want to sleep');
+const cat = new Cat("Luna", "female");
 
- const woman = new Human('human', 'Lena', 'female', 'Hello', 2, 2, man.name );
- 
- const cat = new Animal('cat', 'Luna', 'female','murrr');
-  
- const dog = new Animal('dog','Rasti','male','woof-woof');
+const dog = new Dog("Rasti", "male");
 
- const catWoman = new Human('catwoman','Selina', 'female', cat.saying);
+const catWoman = new CatWoman("Selina");
 
- [man, woman, cat, dog, catWoman].forEach(inhabitant => print(inhabitant.getMeaning().join('; ')))
+[man, woman, cat, dog, catWoman].forEach((inhabitant) =>
+  print(inhabitant.getMeaning())
+);
